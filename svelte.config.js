428483683436d6e1dc4689548cbab3c6de7bf316
k! <dvrchipro+klwpgk@gmail.com>
2025-10-19 15:59:01 +0200

    Updates dependencies and configuration
    Updates project dependencies to their latest versions, including Threlte, Svelte, and Rapier.
    
    Removes sequential preprocessor and configures static adapter, streamlining the build process.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
-import adapter from '@sveltejs/adapter-static'; // MODIFIÉ
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import seqPreprocessor from 'svelte-sequential-preprocessor';
-import { preprocessThrelte } from '@threlte/preprocess';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: seqPreprocessor([preprocess(), preprocessThrelte()]),
+    preprocess: preprocess(),
     kit: {
         adapter: adapter({
             // default options are fine
