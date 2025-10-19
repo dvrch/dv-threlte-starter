1da99433d2bfd983dda49376d57b486c785111cd
k! <dvrchipro+klwpgk@gmail.com>
2025-10-19 17:31:24 +0200

    Updates Threlte and Svelte Preprocess dependencies
    Upgrades @threlte/core to the latest version and svelte-preprocess, including the addition of typescript.
    This update ensures compatibility and leverages the newest features.
    
    Additionally, integrates a basic 3D scene with a cube, camera controls, and lighting, enhancing the visual experience.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,22 @@
 	import { spring } from 'svelte/motion';
 	import { degToRad } from 'three/src/math/MathUtils';
 
+	
+</script>
+
+<div>
+	<Canvas>
+		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+		</T.PerspectiveCamera>
+
+		<T.DirectionalLight castShadow position={[3, 10, 10]} />
+		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+		<T.AmbientLight intensity={0.2} />
+
 		<!-- Cube -->
 		<T.Group>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-
+			<T.Mesh position.y={0.5} castShadow>
 				<T.BoxGeometry />
 				<T.MeshStandardMaterial color="#333333" />
 			</T.Mesh>
