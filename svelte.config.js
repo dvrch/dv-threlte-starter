b55936f5a7b6ea14d0b4e8b89c7826d0124b8677
k! <dvrchipro+klwpgk@gmail.com>
2025-10-19 15:37:53 +0200

    feat: Add GitHub Actions workflow for Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // MODIFIÉ
 import preprocess from 'svelte-preprocess';
 import seqPreprocessor from 'svelte-sequential-preprocessor';
 import { preprocessThrelte } from '@threlte/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: seqPreprocessor([preprocess(), preprocessThrelte()]),
-	kit: {
-		adapter: adapter()
-	}
+    preprocess: seqPreprocessor([preprocess(), preprocessThrelte()]),
+    kit: {
+        adapter: adapter({
+            // default options are fine
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
