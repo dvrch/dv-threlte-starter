e7d1d26a234c2be435b309cb5465cbb286c5d305
k! <dvrchipro+klwpgk@gmail.com>
2025-10-21 22:55:09 +0200

    feat: Initialize Sverdle game and update project structure
    This commit introduces the initial version of the Sverdle game, a Wordle clone built with SvelteKit. It also includes a significant restructuring of the project, removing the previous 3D scene and setting up a new SvelteKit application structure.
    
    ### Features
    - **Sverdle Game:** Added the core logic and components for the Sverdle game, including game state management, word validation, and UI.
    - **New Routes:** Created new routes for the game, about page, and how-to-play instructions.
    - **UI Components:** Added new UI components like Header, Counter, and Keyboard.
    
    ### Project Structure
    - **Dependencies:** Added new dependencies like `@fontsource/fira-mono`, `@neoconfetti/svelte`, and `@sveltejs/adapter-node`.
    - **Vite Config:** Updated the `dev` script to expose the server to the network.
    - **Cleanup:** Removed old files and the previous 3D scene.
    - **Static Assets:** Added new images and static files for the application.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,59 @@
 <script>
-	import '../app.css';
+	import Header from './Header.svelte';
+	import './styles.css';
 </script>
 
-<slot />
+<svelte:head>
+    <title>Sverdle</title>
+    <script async defer src="https://demo.tianji.msgbyte.com/tracker.js" data-website-id="cm03x03dzbxnpevvrpg8zyex4"></script>
+</svelte:head>
+
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>© 2023 - PRESENT</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,59 @@
 <script>
-	import { Canvas, T } from '@threlte/core';
-	import { OrbitControls } from '@threlte/extras';
-	import { spring } from 'svelte/motion';
-	import { degToRad } from 'three/src/math/MathUtils';
-
-	
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
-<div>
-	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
-		</T.PerspectiveCamera>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Wordle clone made with SvelteKit" />
+</svelte:head>
 
-		<T.DirectionalLight castShadow position={[3, 10, 10]} />
-		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-		<T.AmbientLight intensity={0.2} />
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
 
-		<!-- Cube -->
-		<T.Group>
-			<T.Mesh position.y={0.5} castShadow>
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
-			</T.Mesh>
-		</T.Group>
+		to your new<br />SvelteKit app
+	</h1>
 
-		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
-	</Canvas>
-</div>
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
 
 <style>
-	div {
-		height: 100vh;
-		width: 100vw;
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
 	}
 </style>
