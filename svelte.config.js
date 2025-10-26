c6369d907c26a0be88852ab07ddd39666abda193
k! <dvrchipro+klwpgk@gmail.com>
2025-10-26 16:13:42 +0100

    "feat: Enable dynamic deployment to Vercel and       GitHub Pages"

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const isGhPages = process.env.DEPLOY_TARGET === 'GH_PAGES';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: isGhPages
+			? adapterStatic({
+					pages: 'build',
+					assets: 'build',
+					fallback: 'index.html',
+					precompress: false,
+					strict: true
+			  })
+			: adapterAuto(),
+		paths: {
+			base: isGhPages ? '/dv-threlte-starter' : ''
+		}
 	}
 };
 
