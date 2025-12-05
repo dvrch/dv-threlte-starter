d6d80ef541484bc68c628696c1f17ee8d057432c
k! <dvrchipro+klwpgk@gmail.com>
2025-12-05 21:28:04 +0100

    chore: update app configurations bkd.films

--- backend/Base_threlte_dv/apps.py
+++ backend/Base_threlte_dv/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BaseThrelteDvConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Base_threlte_dv'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "backend.Base_threlte_dv"
