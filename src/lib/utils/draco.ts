b3a400bbe6920bed4a3a58d79c1e0a70e7efe56d
k! <dvrchipro+klwpgk@gmail.com>
2025-12-12 22:12:16 +0100

    supp

--- src/lib/utils/draco.ts
+++
@@ -1,9 +0,0 @@
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
-import { useLoader } from '@threlte/core';
-
-// Crée une instance de DRACOLoader mise en cache globalement
-export const dracoLoader = useLoader(DRACOLoader, {
-  extend: (loader) => {
-    loader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
-  }
-});
