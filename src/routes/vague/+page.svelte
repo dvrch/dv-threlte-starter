e6065c6dbfa4cafcf86dbacb90d90e1282344ef5
k! <dvrchipro+klwpgk@gmail.com>
2025-12-15 04:57:39 +0100

    fix(vague): Correctly parent OrbitControls to the camera

--- src/routes/vague/+page.svelte
+++ src/routes/vague/+page.svelte
@@ -5,14 +5,11 @@
 	import { OrbitControls } from '@threlte/extras';
 </script>
 
-<h1 style="position: absolute; top: 10px; left: 10px; z-index: 999; color: red; font-size: 2rem;">
-	TEST: VAGUE PAGE LOADED
-</h1>
-
 <Canvas>
-	<T.PerspectiveCamera position={[0, 1, 2]} makeDefault />
+	<T.PerspectiveCamera position={[0, 1, 2]} makeDefault>
+		<OrbitControls />
+	</T.PerspectiveCamera>
 	<T.AmbientLight intensity={0.5} />
 	<T.DirectionalLight position={[10, 10, 5]} />
-	<OrbitControls />
 	<Vaguend />
 </Canvas>
