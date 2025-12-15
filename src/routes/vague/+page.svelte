91a55bc54c9e776aa0d1849c89fd3dc061c020ea
k! <dvrchipro+klwpgk@gmail.com>
2025-12-15 12:00:25 +0100

    refactor: Dynamically load canvas component and add About store

--- src/routes/vague/+page.svelte
+++ src/routes/vague/+page.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
-	import Vaguend from './vaguend.svelte';
 	import { T } from '@threlte/core';
-	import { Canvas } from '@threlte/core';
 	import { OrbitControls } from '@threlte/extras';
+	import Vaguend from './vaguend.svelte';
+	import About from '../stores/About.svelte';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	let Canvas;
+
+	let {
+		position = [0, 0, 0],
+		rotation = [0, 0, 0],
+		scale = 0.8
+	}: {
+		position?: [number, number, number];
+		rotation?: [number, number, number];
+		scale?: number | [number, number, number];
+	} = $props();
+
+	onMount(async () => {
+		if (browser) {
+			const threlte = await import('@threlte/core');
+			Canvas = threlte.Canvas;
+		}
+	});
 </script>
 
-<Canvas>
-	<T.PerspectiveCamera position={[0, 1, 2]} makeDefault>
-		<OrbitControls />
-	</T.PerspectiveCamera>
-	<T.AmbientLight intensity={0.5} />
-	<T.DirectionalLight position={[10, 10, 5]} />
-	<Vaguend />
-</Canvas>
+{#if browser && Canvas}
+	<Canvas>
+		<Vaguend {position} {rotation} {scale} />
+		<About />
+	</Canvas>
+{/if}
