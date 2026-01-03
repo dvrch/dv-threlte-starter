da8a7c6ac71c1a0a12fe4c91ef8020eb180f2c53
k! <dvrchipro+klwpgk@gmail.com>
2026-01-03 23:18:23 +0100

    Fix: configure base path pour GitHub Pages (/dv-threlte-starter)

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV === 'development';
+const basePath = process.env.DEPLOY_TARGET === 'GH_PAGES' ? '/dv-threlte-starter' : '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: basePath
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
